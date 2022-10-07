193a544c796255e53b419f9d8b7e0fa86dde312d
Thanagorn Ruangsit <truangsi@z2t16c1.42bangkok.com>
2022-10-07 16:39:26 +0700

    split

--- ft_split.c
+++ ft_split.c
@@ -12,35 +12,39 @@
 
 #include "libft.h"
 
-char **get_mal(char const *s, char c)
+char	**get_mal(char const *s, char c)
 {
-	int slot;
-	int i;
-	char **arr;
+	int		count;
+	int		slot;
+	int		i;
+	char	**arr;
 
+	count = 0;
 	i = 1;
-	slot = 1;
-	while (s[i])
+	slot = 0;
+
+	while (s[i] && ft_strlen(s) != 0)
 	{
+		if (s[0] != c && i == 1)
+			slot++;
 		if (s[i - 1] == c && s[i] != c)
 			slot++;
 		i++;
 	}
 	slot++;
-	arr = (char **)malloc(slot * sizeof(char *));
+	arr = (char **) malloc(slot * sizeof(char *));
 	return (arr);
 }
 
-char **insert(char const *s, char c, int slot, char **arr)
+char	**insert(char const *s, char c, int slot, char **arr)
 {
-	int i;
-	int j;
-	int count;
+	int	i;
+	int	j;
+	int	count;
 
 	i = 0;
 	j = 0;
 	count = 0;
-	arr[slot] = NULL;
 	while (i < slot)
 	{
 		while (s[count] == c)
@@ -55,20 +59,20 @@ char **insert(char const *s, char c, int slot, char **arr)
 		j = 0;
 		i++;
 	}
-	arr[i] = NULL;
+	arr[slot] = NULL;
 	return (arr);
 }
 
-char **allocate(char const *s, char c, char **arr)
+char	**allocate(char const *s, char c, char **arr)
 {
-	int count;
-	int slot;
-	size_t i;
+	int		count;
+	int		slot;
+	size_t	i;
 
 	i = 0;
 	count = 0;
 	slot = 0;
-	while (i < ft_strlen(s))
+	while (i < ft_strlen(s) )
 	{
 		while (s[i] != c && s[i] != '\0')
 		{
@@ -77,7 +81,8 @@ char **allocate(char const *s, char c, char **arr)
 		}
 		if (count > 0)
 		{
-			arr[slot] = (char *)malloc((count + 1) * sizeof(char));
+			// arr[slot] = (char *) malloc((count  + 1) * sizeof(char));
+			arr[slot] = (char *) malloc(count  + 1);
 			slot++;
 			count = 0;
 		}
@@ -87,10 +92,16 @@ char **allocate(char const *s, char c, char **arr)
 	return (arr);
 }
 
-char **ft_split(char const *s, char c)
+char	**ft_split(char const*s, char c)
 {
-	char **arr;
+	int		count;
+	int		slot;
+	size_t	i;
+	char	**arr;
 
+	slot = 0;
+	count = 0;
+	i = 0;
 	if (!s)
 		return (NULL);
 	arr = get_mal(s, c);
@@ -99,11 +110,15 @@ char **ft_split(char const *s, char c)
 	arr = allocate(s, c, arr);
 	return (arr);
 }
-
-/*int main(void)
+/*
+int	main(void)
 {
-	char *str = "tripouille  42  ";
-	char c = ' ';
+	char	*str = "  tripouille  42  ";
+	char	c = ' ';
+	// char	*str = " tripouille";
+	// char	c = ' ';
+	// char	*str = "";
+	// char	c = 'z';
 	// char	*str = "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse";
 	// char	c = ' ';
 	// char	*str = "tripouille";
@@ -111,16 +126,30 @@ char **ft_split(char const *s, char c)
 
 	// char	*str = strdup("tripouille");
 	// char	c = ' ';
-	char **ret;
+	char	**ret;
 
 	ret = ft_split(str, c);
-	printf("ret0: %s\n", ret[0]);
-	printf("ret1: %s\n", ret[1]);
-	printf("ret2: %s\n", ret[2]);
+
+	int i = 0;
+	// while (i < 10)
+	// {
+	// 	printf("this is ret ==> [%s]\n", ret[i]);
+	// 	i++;
+	// }
+	printf("ret0: |%s|\n", ret[0]);
+	printf("ret0: |%s|\n", ret[1]);
+	printf("ret0: |%s|\n", ret[2]);
+	printf("ret0: |%s|\n", ret[3]);
+	free(ret);
+	// printf("ret1: |%s|\n", ret[1]);
+	// printf("ret2: %s\n", ret[2]);
+	// printf("ret3: %s\n", ret[3]);
+	// printf("ret4: %s\n", ret[4]);
 	// puts("-----------");
 	// free(ret[0]);
 	// free(ret);
 	// printf("ret0: %s\n", ret[0]);
 	// printf("ret1: %s\n", ret[1]);
 	// printf("ret: %s\n", ret[2]);
-}*/
+}
+*/
